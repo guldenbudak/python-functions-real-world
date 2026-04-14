5380617fffec7b73ccf912aa730bc7a58df140d3
gülden budak <gulden@gulden-MacBook-Air.local>
2026-04-14 17:06:57 +0300

    update hotel reservation summary function

--- hotel_reservation_summary.py
+++ hotel_reservation_summary.py
@@ -1,33 +1,25 @@
-def reservation_summary(name, day, pay):
+def get_reservation_price(day, pay):
     return day * pay
 
 
-def is_breakfast_included(breakfast_included):
-    if breakfast_included == "y":
-        extra = reservation_summary(name, day, pay) + 500
-        return extra
-
-    elif breakfast_included == "n":
-        return reservation_summary(name, day, pay)
-
-    else:
-        return "Invalid character entry! "
+def get_price_with_breakfast(day):
+    extra = get_reservation_price(day, pay) + 500 * day
+    return extra
 
 
 name = input("Enter your name: ")
-
 day = int(input("Please enter how many days you wish to stay: "))
+
 if day <= 0:
     print("Sorry, incorrect entry!")
 
 pay = int(input("How much would you like to pay: "))
 
 breakfast_included = input("Do you have a breakfast? (y/n): ")
-if breakfast_included == "y":
-    reservation_pay = reservation_summary(name, day, pay)
-    breakfast = is_breakfast_included(breakfast_included)
-    print(f"Merhaba, {name} {day} gecelik konaklama tutarınız {breakfast} TL'dir.")
 
+if breakfast_included == "y":
+    total_price = get_price_with_breakfast(day)
+    print(f"Merhaba, {name} {day} gecelik konaklama tutarınız {total_price} TL'dir.")
 else:
-    reservation_pay = reservation_summary(name, day, pay)
+    reservation_pay = get_reservation_price(day, pay)
     print(f"Merhaba, {name} {day} gecelik konaklama tutarınız {reservation_pay} TL'dir.")
